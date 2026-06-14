d5c9d8b6bbe88ab257a99eef4d21c12d1eeacef3
Monsho <cinderella.cage@gmail.com>
2026-06-14 10:01:49 +0900

    追加忘れ

---
+++ SampleLib12/include/sl12/debug.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <sl12/util.h>
+#include <sl12/string_util.h>
+
+namespace sl12
+{
+	struct DebugName
+	{
+#ifdef _DEBUG
+		std::string name;
+
+		void SetDebugName(const std::string& n, ID3D12Object* pObj = nullptr)
+		{
+			name = n;
+			if (pObj) pObj->SetName(StringToWString(name).c_str());
+		}
+		const std::string& GetDebugName() const
+		{
+			return name;
+		}
+#else
+		void SetDebugName(const std::string& n, ID3D12Object* pObj = nullptr)
+		{
+		}
+		const std::string& GetDebugName() const
+		{
+			return std::string();
+		}
+#endif
+	};
+
+}	// namespace sl12
+
+//	EOF
